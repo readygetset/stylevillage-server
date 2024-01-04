9b5f8c7056d9a7e3f58f356eff20ff5ac0b44e0a
배성진[ 학부재학 / 컴퓨터학과 ] <qotjdwls66@korea.edu>
2024-01-04 16:43:36 +0900

    feat: create like entity

---
+++ src/entity/like.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column } from 'typeorm';
+import BaseTime from './baseTime.entity';
+import User from './user.entity';
+import Clothes from './clothes.entity';
+
+@Entity()
+export default class Like extends BaseTime {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @ManyToOne(() => User, {
+    cascade: ['insert', 'recover', 'update'],
+  })
+  user!: User;
+
+  @ManyToOne(() => Clothes, {
+    cascade: ['insert', 'recover', 'update'],
+  })
+  clothes!: Clothes;
+
+  @Column({
+    type: 'boolean',
+    default: false,
+    nullable: false,
+    comment: '좋아요 여부',
+  })
+  is_liked!: boolean;
+}
