4f174a02c3aedb3c7cafacaded828ff0af9c29d9
Dcaffein <hj02117@korea.ac.kr>
2024-01-06 14:19:08 +0900

    fix:로그인 되어있어야 옷 생성가능하게 수정

--- src/controller/clothes/controller.ts
+++ src/controller/clothes/controller.ts
@@ -12,6 +12,11 @@ export const createClothes: RequestHandler = async (req, res, next) => {
     const { closet, category, season, status, is_open, name, tag, image } =
       req.body;
 
+    if (!req.user) {
+      res.status(401).json({ error: '로그인이 필요한 기능입니다.' });
+      return;
+    }
+
     if (!closet || !name) {
       throw new BadRequestError('closet,name are essential');
     }
@@ -19,15 +24,15 @@ export const createClothes: RequestHandler = async (req, res, next) => {
     //Enum fields 유효성 검사
     if (category) {
       if (!Object.values(Category).includes(category))
-        throw new BadRequestError('category is not valid');
+        throw new BadRequestError('category is invalid');
     }
     if (season) {
       if (!Object.values(Season).includes(season))
-        throw new BadRequestError('season is not valid');
+        throw new BadRequestError('season is invalid');
     }
     if (status) {
       if (!Object.values(Status).includes(status))
-        throw new BadRequestError('status is not valid');
+        throw new BadRequestError('status is invalid');
     }
 
     const clothesInfo: CreateClothesReq = {
