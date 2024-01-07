1a48fa4edb6c09ca06a7feb5e41644bdc9017389
daseul <ek0539@naver.com>
2024-01-07 22:38:46 +0900

    fix: service의 try catch문 삭제

--- src/service/closet.service.ts
+++ src/service/closet.service.ts
@@ -5,44 +5,38 @@ import ClothesRepository from '../repository/clothes.repository';
 import getClosetRes from '../type/closet/getCloset.res';
 import getClosetClothes from '../type/clothes/getClosetClothes';
 import userRes from '../type/user/user.res';
-import { InternalServerError } from '../util/customErrors';
 
 export default class ClosetService {
   static async getCloset(closetId: number): Promise<getClosetRes> {
-    try {
-      const closet: Closet = await ClosetRepository.findOneByClosetId(closetId);
-      const clothes: Clothes[] =
-        await ClothesRepository.findByClosetId(closetId);
+    const closet: Closet = await ClosetRepository.findOneByClosetId(closetId);
+    const clothes: Clothes[] = await ClothesRepository.findByClosetId(closetId);
 
-      const clothesInfo: Array<getClosetClothes> = clothes.map((clothes) => {
-        const eachClothes: getClosetClothes = {
-          id: clothes.id,
-          category: clothes.category,
-          season: clothes.season,
-          status: clothes.status,
-          name: clothes.name,
-          tag: clothes.tag,
-          image: clothes.image,
-        };
-        return eachClothes;
-      });
-
-      const ownerInfo: userRes = {
-        id: closet.owner.id,
-        username: closet.owner.username,
-        nickname: closet.owner.nickname,
+    const clothesInfo: Array<getClosetClothes> = clothes.map((clothes) => {
+      const eachClothes: getClosetClothes = {
+        id: clothes.id,
+        category: clothes.category,
+        season: clothes.season,
+        status: clothes.status,
+        name: clothes.name,
+        tag: clothes.tag,
+        image: clothes.image,
       };
+      return eachClothes;
+    });
 
-      const closetInfo: getClosetRes = {
-        id: closet.id,
-        name: closet.name,
-        owner: ownerInfo,
-        clothes: clothesInfo,
-      };
+    const ownerInfo: userRes = {
+      id: closet.owner.id,
+      username: closet.owner.username,
+      nickname: closet.owner.nickname,
+    };
 
-      return closetInfo;
-    } catch (error) {
-      throw new InternalServerError('옷장 정보를 불러오는 데 실패했습니다.');
-    }
+    const closetInfo: getClosetRes = {
+      id: closet.id,
+      name: closet.name,
+      owner: ownerInfo,
+      clothes: clothesInfo,
+    };
+
+    return closetInfo;
   }
 }
