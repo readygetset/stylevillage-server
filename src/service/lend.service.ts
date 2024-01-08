bb99528fa9176f45f1db7dcde07bfce9c38c252a
Dcaffein <hj02117@korea.ac.kr>
2024-01-08 17:48:51 +0900

    다른 브랜치 수정을 위해 중간저장

--- src/service/lend.service.ts
+++ src/service/lend.service.ts
@@ -1,3 +1,6 @@
+import Lend from '../entity/lend.entity';
+import createLendReq from '../type/lend/createLend.req';
+
 export default class lendService {
-    static async createLend(lend : )
+  static async createLend(lend: createLendReq): Promise<Lend> {}
 }
---
+++ src/type/lend/createLend.req.ts
@@ -0,0 +1,9 @@
+export default interface createLendReq {
+  clothes: number;
+  price: number;
+  statrDate: Date;
+  endDate: Date;
+  lender: number;
+  loanee: number;
+  review: string;
+}
