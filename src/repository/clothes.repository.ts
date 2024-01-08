9a2a51a28fbd8cd51c66836165e888114d4c3d63
foura1201 <84717712+foura1201@users.noreply.github.com>
2024-01-08 15:03:20 +0900

    fix: get owner with clothes

--- src/repository/clothes.repository.ts
+++ src/repository/clothes.repository.ts
@@ -4,7 +4,10 @@ import { BadRequestError } from '../util/customErrors';
 
 const ClothesRepository = AppDataSource.getRepository(Clothes).extend({
   async findOneByClothesId(id: number): Promise<Clothes> {
-    return this.findOneBy({ id }).then((clothes) => {
+    return this.findOne({
+      where: { id },
+      relations: { closet: { owner: true } },
+    }).then((clothes) => {
       if (!clothes) throw new BadRequestError('등록되어있지 않은 의류입니다.');
       return clothes;
     });
