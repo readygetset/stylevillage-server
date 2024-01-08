71ee079e6a53fbbc1b6d6636714760c495911351
Dcaffein <hj02117@korea.ac.kr>
2024-01-08 15:14:39 +0900

    파일생성

---
+++ src/type/apply/approveApply.req.ts
@@ -0,0 +1,15 @@
+export default interface approveApplyReq {
+  id: number;
+  user: number;
+  clothes: number;
+  detail: string;
+}
+
+// Table apply {
+//     id int [pk, increment]
+//     user int [ref: > user.id]
+//     clothes int [ref: > clothes.id]
+//     is_accepted boolean
+//     is_rejected boolean
+//     detail varchar
+//   }
