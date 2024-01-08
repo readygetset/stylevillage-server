8fa3a16d365f31cde0b51366968d3c7719a8ec2b
daseul <ek0539@naver.com>
2024-01-08 17:54:17 +0900

    feat: modify-closet-api

--- src/controller/closet/router.ts
+++ src/controller/closet/router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { postCloset } from './controller';
-
 import { getCloset } from './controller';
+import { modifyCloset } from './controller';
 
 const closetRouter = Router();
 
 closetRouter.post('/', postCloset);
 closetRouter.get('/:closetId', getCloset);
+closetRouter.put('/:closetId', modifyCloset);
 
 export default closetRouter;
