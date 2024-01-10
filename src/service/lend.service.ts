0c7356b68b315bcbf10eef7f2b70b82c25a2306e
Dcaffein <hj02117@korea.ac.kr>
2024-01-11 02:36:20 +0900

    대여를 최신 것 부터 볼 수 있게 변경

--- src/service/lend.service.ts
+++ src/service/lend.service.ts
@@ -5,12 +5,18 @@ export default class LendService {
   static async getLendsAsLender(userId: number): Promise<Lend[]> {
     return LendRepository.find({
       where: { lender: { id: userId } },
+      order: {
+        createdAt: 'DESC',
+      },
     });
   }
 
   static async getLendAsLoanee(userId: number): Promise<Lend[]> {
     return LendRepository.find({
       where: { loanee: { id: userId } },
+      order: {
+        createdAt: 'DESC',
+      },
     });
   }
 }
