2970bdf8b5f33af924c1447fd772009e4c59e237
배성진[ 학부재학 / 컴퓨터학과 ] <qotjdwls66@korea.edu>
2024-01-12 15:56:46 +0900

    fix:Resolve merge conflict

--- src/controller/router.ts
+++ src/controller/router.ts
@@ -4,6 +4,8 @@ import authRouter from './auth/router';
 import closetRouter from './closet/router';
 import clothesRouter from './clothes/router';
 import lendRouter from './lend/router';
+import reviewRouter from './review/router';
+import applyRouter from './apply/router';
 import wishRouter from './wish/router';
 
 const router = Router();
@@ -13,6 +15,8 @@ router.use('/auth', authRouter);
 router.use('/closet', closetRouter);
 router.use('/clothes', clothesRouter);
 router.use('/lend', lendRouter);
+router.use('/review', reviewRouter);
+router.use('/apply', applyRouter);
 router.use('/wish', wishRouter);
 
 export default router;
