708be663360187fe154e531a4d667939726fb47b
daseul <ek0539@naver.com>
2024-01-12 13:37:03 +0900

    getLoaneeById 레포지토리 삭제

--- src/repository/lend.repository.ts
+++ src/repository/lend.repository.ts
@@ -1,8 +1,6 @@
 import AppDataSource from '../config/dataSource';
 import Lend from '../entity/lend.entity';
 import ReviewRes from '../type/lend/review.res';
-import UserRes from '../type/user/user.res';
-import { BadRequestError } from '../util/customErrors';
 
 const LendRepository = AppDataSource.getRepository(Lend).extend({
   async findByLenderId(lender: number): Promise<Lend[]> {
@@ -31,29 +29,15 @@ const LendRepository = AppDataSource.getRepository(Lend).extend({
     });
   },
 
-  async getLoaneeById(id: number): Promise<UserRes> {
-    const lend = await this.findOne({
-      where: { id },
-      relations: { loanee: true },
-    }).then((lend) => {
-      if (!lend) throw new BadRequestError('대여 내역이 존재하지 않습니다.');
-      return lend;
-    });
-    const loanee: UserRes = {
-      id: lend.loanee.id,
-      username: lend.loanee.username,
-      nickname: lend.loanee.nickname,
-    };
-    return loanee;
-  },
-
   async getReviewByClothesId(clothesId: number): Promise<ReviewRes[]> {
     const lends = await this.findByClothesId(clothesId);
     const reviewsPromises = lends.map(async (lend: Lend) => {
       if (!lend || !lend.review || !lend.id) return undefined;
       else {
-        const reviewer = await LendRepository.getLoaneeById(lend.id);
-        const review: ReviewRes = { review: lend.review, reviewer: reviewer };
+        const review: ReviewRes = {
+          review: lend.review,
+          reviewer: lend.loanee,
+        };
         return review;
       }
     });
